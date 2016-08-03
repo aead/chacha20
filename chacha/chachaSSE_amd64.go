91994aef74bf142aeb63794e25afd05d81f83220
Andreas Auernhammer <aead@mail.de>
2016-08-03 23:46:39 +0200

    Fixed version depended compilation

--- chacha/chachaSSE_amd64.go
+++ chacha/chachaSSE_amd64.go
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a license that can be
 // found in the LICENSE file.
 
-// +build amd64, !gccgo, !appengine
+// +build !go1.7
+// +build !gccgo, !appengine
 
 package chacha
 
