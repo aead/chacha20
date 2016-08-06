87e3e3eb0246584e1e228dd3c2a5e508cf48a2fb
Andreas Auernhammer <aead@mail.de>
2016-08-06 16:11:15 +0200

    Fixed build fail on 386

--- chacha/chacha_test.go
+++ chacha/chacha_test.go
@@ -12,44 +12,6 @@ import (
 	"testing"
 )
 
-var testFuncs = map[string]func(*testing.T){
-	"TestCore":         TestCore,
-	"TestXorBlocks":    TestXorBlocks,
-	"TestXORKeyStream": TestXORKeyStream,
-}
-
-func TestSSE2(t *testing.T) {
-	SSSE3 := useSSSE3
-	AVX2 := useAVX2
-	useSSSE3, AVX2 = false, false
-	for _, v := range testFuncs {
-		v(t)
-	}
-	useSSSE3, useAVX2 = SSSE3, AVX2
-}
-
-func TestSSSE3(t *testing.T) {
-	if !useSSSE3 {
-		t.Log("CPU does not support SSSE3 - cannot test SSSE3")
-	}
-	AVX2 := useAVX2
-	AVX2 = false
-	for _, v := range testFuncs {
-		v(t)
-	}
-	useAVX2 = AVX2
-}
-
-func TestAVX2(t *testing.T) {
-	if !useAVX2 {
-		t.Log("CPU does not support AVX2 - cannot test AVX2")
-	}
-	if v := runtime.Version(); !strings.HasPrefix(v, "go1.7") {
-		t.Logf("Go version: %s < go1.7 - cannot test AVX2", v)
-	}
-	// if useAVX2 && version >= 1.7 -> AVX2 is always used.
-}
-
 func fromHex(s string) []byte {
 	b, err := hex.DecodeString(s)
 	if err != nil {
